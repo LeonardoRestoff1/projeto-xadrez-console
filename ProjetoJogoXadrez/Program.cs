7ce9d5f266c6afa8a6e350e0f7083f83059260ab
Leonardo Restoff <leonardorestoff1@gmail.com>
2024-12-10 13:16:54 -0300

    Cirando a classe PosiçãoXadrez

--- ProjetoJogoXadrez/Program.cs
+++ ProjetoJogoXadrez/Program.cs
@@ -9,23 +9,14 @@ namespace ProjetoJogoXadrez
         static void Main(string[] args)
         {
 
-            try { 
 
-            Tabuleiro tab = new Tabuleiro(8, 8);
+            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
 
-            tab.colocarPeca(new Torre(Cor.Preta, tab), new Posicao(0, 0));
-            tab.colocarPeca(new Torre(Cor.Preta, tab), new Posicao(1, 3));
-            tab.colocarPeca(new Rei(Cor.Preta, tab), new Posicao(2, 9));
+            Console.WriteLine(pos);
 
+            Console.WriteLine(pos.toPosicao());
 
-            Tela.imprimirTabuleiro(tab);
-            }
-            catch (TabuleiroException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-
-        Console.WriteLine();
+            Console.WriteLine();
 
 
            
